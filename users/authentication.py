ea4ae6fa3a29c461a7d8729dafe67e5f6208af89
Tigran Petrosyan <tigr-080807@mail.ru>
2025-11-10 00:32:02 +0500

    Исправление ошибок

--- users/authentication.py
+++ users/authentication.py
@@ -18,19 +18,3 @@ class EmailAuthBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except user_model.DoesNotExist:
             return None
-
-class EmailAuthBackend(BaseBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = User.objects.get(email=username)
-            if user.check_password(password):
-                return user
-            return None
-        except (User.DoesNotExist, User.MultipleObjectsReturned):
-            return None
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
