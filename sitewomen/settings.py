e19c91ab69d24dd328ac713f96f6b7c9f5b7fd50
Tigran Petrosyan <tigr-080807@mail.ru>
2025-11-18 18:52:52 +0500

    GitHub-аутентификаци

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -152,3 +152,19 @@ SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'users.User'
+
+SOCIAL_AUTH_GITHUB_KEY = 'Ov23lilMqoOKhTcbToX6'
+SOCIAL_AUTH_GITHUB_SECRET = 'cbe15e72ef74fd5db65aecdb38f2adda78e122a1'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
---
+++ users/pipeline.py
@@ -0,0 +1,7 @@
+from email.headerregistry import Group
+
+
+def new_users_handler(backend, user, response, *args, **kwargs):
+    group = Group.objects.filter(name='social')
+    if len(group):
+        user.groups.add(group[0])
