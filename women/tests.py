05d65798d2f21a667c6da083824a9b1179558181
Tigran Petrosyan <tigr-080807@mail.ru>
2025-11-22 19:13:53 +0500

    Использование БД в unittest

--- women/tests.py
+++ women/tests.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse, reverse_lazy
 
+from women.models import Women
+
 
 # Create your tests here.
 class WomenTest(TestCase):
@@ -21,6 +23,30 @@ class WomenTest(TestCase):
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, redirect_url)
+        
+    def test_data_main(self):
+        w = Women.published.all()
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerySetEqual(response.context_data['posts'], w[:5])
+        
+    def test_paginate_main(self):
+        path = reverse('home')
+        page = 1
+        paginate_by = 5
+        response = self.client.get(path + f'?page={page}')
+        w = Women.published.all()
+        self.assertQuerySetEqual(
+            response.context_data['posts'], 
+            w[(page - 1) * paginate_by:page * paginate_by]
+        )
+        
+    def test_content_post(self):
+        w = Women.published.get(pk=1)
+        path = reverse('post', args=[w.slug])
+        response = self.client.get(path)
+        self.assertEqual(w.content, response.context_data['posts'].content)
+            
         
     def tearDown(self):
         pass
